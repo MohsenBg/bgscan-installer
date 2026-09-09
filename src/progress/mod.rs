600df902da2af52a910c53ab78459ab2d8f41d75
MohsenBg <moh.1380.1393@gmail.com>
2026-09-09 13:52:00 +0330

    Enhance progress bar styling and functionality; add steady tick and custom finish message

--- src/progress/mod.rs
+++ src/progress/mod.rs
@@ -9,16 +9,20 @@ impl Progress {
         let bar = ProgressBar::new(total);
 
         bar.set_style(
-            ProgressStyle::with_template(
-                "{msg}\
-         {bar:40.cyan/blue} {percent:>3}%\n\
-         Downloaded: {bytes}/{total_bytes}\n\
-         Speed:      {bytes_per_sec}\n\
-         Elapsed:    {elapsed_precise}\n\
-         Remaining:  {eta_precise}",
-            )
-            .unwrap(),
+            ProgressStyle::with_template(concat!(
+                "\n",
+                "  {spinner:.green} {msg:.bold}\n",
+                "  {bar:40.green/black} {percent:>3}%\n",
+                "  {bytes:>10.yellow} / {total_bytes:<10.yellow}",
+                "   {bytes_per_sec:.cyan}\n",
+                "  ⏱  {elapsed_precise:.dim}   ⏳ {eta_precise:.dim}\n",
+            ))
+            .unwrap()
+            .progress_chars("█▓░")
+            .tick_chars("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"),
         );
+
+        bar.enable_steady_tick(std::time::Duration::from_millis(80));
 
         Self { bar }
     }
@@ -28,6 +32,6 @@ impl Progress {
     }
 
     pub fn finish(&self) {
-        self.bar.finish();
+        self.bar.finish_with_message("done ✔");
     }
 }
